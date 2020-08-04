8f7cc142a16ad10d78047167e63335b31dcfb28e
Huy-Ngo <duchuy29092000@gmail.com>
2020-08-04 10:52:22 +0700

    Use id for title to avoid long div

--- gen-index.js
+++ gen-index.js
@@ -4,6 +4,18 @@ function check() {
     return els.length > 0;
 }
 
+/* Convert id to title case */
+function getTitle(elId) {
+    elId = elId.split('-');
+    elId = elId.map(word => {
+        word = word.split('');
+        word[0] = word[0].toUpperCase();
+        word = word.join('');
+        return word;
+    });
+   return elId.join(' ');
+}
+
 /* Get heading level, link to heading, and the title for each heading */
 function getSectionList() {
     let nodes = document.querySelectorAll('h1, h2, h3, a, span, div');
@@ -23,7 +35,7 @@ function getSectionList() {
         }
         let level = levelMap[node.tagName.toLowerCase()];
         let link = '#' + node.id;
-        let title = node.innerText;
+        let title = getTitle(node.id);
         sectionList.push({
             'level': level,
             'link': link,
