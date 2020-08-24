1a6cb6655ef9fc5deee7e5009cd3ad3cb607438d
Huy-Ngo <duchuy29092000@gmail.com>
2020-08-24 22:49:51 +0700

    Moving createIndex and modify

--- popup.js
+++ popup.js
@@ -1,8 +1,42 @@
-// let popup = document.getElementById('gen-index');
+const index = document.querySelector('div#gen-index');
+
 window.onload = function (element) {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         chrome.tabs.executeScript(
             tabs[0].id,
-            { file: 'contentScript.js' });
+            { file: 'gen-index.js' });
+        chrome.tabs.executeScript(
+            tabs[0].id,
+            { code: `console.log(${JSON.stringify(index)})`}
+        )
     });
 };
+
+/* Create the index */
+function createIndex(sectionList) {
+    const indexTitle = document.createElement('div');
+    let text = document.createTextNode('Index');
+    indexTitle.appendChild(text);
+    index.appendChild(indexTitle);
+    for (let section of sectionList) {
+        let div = document.createElement('div');
+        let a_link = document.createElement('a');
+        let text = document.createTextNode(section.title);
+        a_link.href = section['link'];
+        a_link.appendChild(text);
+
+        div.appendChild(a_link);
+        div.style['text-indent'] = (20 * section.level) + 'px';
+
+        index.appendChild(div);
+    }
+    return index;
+}
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (sender.tab && request.sections) {
+            res = createIndex(request.sections);
+        }
+        sendResponse({result: res})
+    });
